7b23dd4a777ca9f098efcb518f2f65db955e8f99
Evan Galli <evan_g@orange.fr>
2024-06-11 16:51:47 +0200

    style: Code format

--- generation_code.py
+++ generation_code.py
@@ -12,73 +12,56 @@ print_builtins = False
 
 tableSymboles = TableSymboles()
 
-"""
-affiche une erreur sur la sortie stderr et quitte le programme
-"""
 
-def erreur(s):
-    print("Erreur:", s, file=sys.stderr)
+def erreur(s, line=None):
+    """affiche une erreur sur la sortie stderr et quitte le programme"""
+    print(f"Error (l{line}):" if line else "Error:", s, file=sys.stderr)
     exit(1)
 
 
-"""
-Un print qui ne fonctionne que si la variable.flo afficher_table vaut Vrai.
-(permet de choisir si on affiche le code assembleur ou la table des symboles)
-"""
-
-
 def printifm(*args, **kwargs):
+    """
+    Un print qui ne fonctionne que si la variable.flo afficher_table vaut Vrai.
+    (permet de choisir si on affiche le code assembleur ou la table des symboles)
+    """
     if afficher_code:
         print(*args, **kwargs)
 
 
-"""
-Un print qui ne fonctionne que si la variable.flo afficher_table vaut Vrai.
-(permet de choisir si on affiche le code assembleur ou la table des symboles)
-"""
-
-
 def printift(*args, **kwargs):
+    """
+    Un print qui ne fonctionne que si la variable.flo afficher_table vaut Vrai.
+    (permet de choisir si on affiche le code assembleur ou la table des symboles)
+    """
     if afficher_table:
         print(*args, **kwargs)
 
 
-"""
-Converti un type en une chaîne de caractères
-"""
-
-
 def typeStr(t):
-	if isinstance(t, type):
-		return t.__name__
-	elif isinstance(t, list):
-		return "|".join([typeStr(_t) for _t in t])
-	elif isinstance(t, str):
-		return t
-	else:
-		return str(t)
-
-"""
-Fonction locale, permet d'afficher un commentaire dans le code arm.
-"""
+    """Converti un type en une chaîne de caractères"""
+    if isinstance(t, type):
+        return t.__name__
+    elif isinstance(t, list):
+        return "|".join([typeStr(_t) for _t in t])
+    elif isinstance(t, str):
+        return t
+    else:
+        return str(t)
 
 
 def arm_comment(comment):
+    """Fonction locale, permet d'afficher un commentaire dans le code arm."""
     if comment != "":
-        printifm(
-            "\t\t @ " + comment
-        )  # le point virgule indique le début d'un commentaire en ARM. Les tabulations sont là pour faire jolie.
+        printifm("\t\t @ " + comment)  # le point virgule indique le début d'un commentaire en ARM. Les tabulations sont là pour faire jolie.
     else:
         printifm("")
 
 
-"""
-Affiche une instruction ARM sur une ligne
-Par convention, les derniers opérandes sont nuls si l'opération a moins de 3 arguments.
-"""
-
-
 def arm_instruction(opcode, op1="", op2="", op3="", comment=""):
+    """
+    Affiche une instruction ARM sur une ligne
+    Par convention, les derniers opérandes sont nuls si l'opération a moins de 3 arguments.
+    """
     if op2 == "":
         printifm("\t" + opcode + "\t" + op1 + "\t\t", end="")
     elif op3 == "":
@@ -88,23 +71,20 @@ def arm_instruction(opcode, op1="", op2="", op3="", comment=""):
     arm_comment(comment)
 
 
-"""
-Retourne le nom d'une nouvelle étiquette
-"""
-
-
 def arm_nouvelle_etiquette():
+    """
+    Retourne le nom d'une nouvelle étiquette
+    """
     global num_etiquette_courante
     num_etiquette_courante += 1
     return ".e" + str(num_etiquette_courante)
 
 
-"""
-Affiche le code arm correspondant à tout un programme
-"""
-
-
 def gen_programme(programme):
+    """
+    Affiche le code arm correspondant à tout un programme
+    """
+
     header = """
 .global __aeabi_idiv
 .global __aeabi_idivmod
@@ -142,6 +122,7 @@ def gen_programme(programme):
     arm_instruction("mov", "r0", "#0", "", "")
     arm_instruction("pop", "{fp, pc}", "", "", "")
 
+
 def gen_def_fonction(f):
     tableSymboles.enterFunction(f)
     printifm(f"_{f.name}:")
@@ -151,20 +132,15 @@ def gen_def_fonction(f):
     arm_instruction("pop", "{fp, pc}")
     tableSymboles.quitFunction(f)
 
-"""
-Affiche le code arm correspondant à une suite d'instructions
-"""
-
 
 def gen_listeInstructions(listeInstructions, function=None):
+    """Affiche le code arm correspondant à une suite d'instructions"""
     for instruction in listeInstructions.instructions:
         gen_instruction(instruction, function)
 
 
-"""
-Affiche le code arm correspondant à une instruction
-"""
 def gen_instruction(instruction, function=None):
+    """Affiche le code arm correspondant à une instruction"""
     if type(instruction) == arbre_abstrait.Function:
         inProgram, inBuiltins = tableSymboles.has(instruction.fct)
         if not inProgram and not inBuiltins:
@@ -186,9 +162,9 @@ def gen_instruction(instruction, function=None):
                     gen_ecrire(instruction)
                 else:
                     erreur(f"Builtin function not found {instruction.fct}")
-            
+
         return tableSymboles.returnType(instruction.fct)
-            
+
     elif type(instruction) in [arbre_abstrait.While, arbre_abstrait.If]:
         gen_block_operation(instruction)
     elif type(instruction) == arbre_abstrait.Return:
@@ -204,34 +180,29 @@ def gen_instruction(instruction, function=None):
     return None
 
 
-"""
-Affiche le code arm correspondant au fait d'envoyer la valeur entière d'une expression sur la sortie standard
-"""
 def gen_ecrire(ecrire):
-    arm_instruction(
-        "pop", "{r1}", "", "", ""
-    )  # on dépile la valeur d'expression sur r1
-    arm_instruction("ldr", "r0", "=.LC1", "", "")
-    arm_instruction(
-        "bl", "printf", "", "", ""
-    )  # on envoie la valeur de r1 sur la sortie standard
+    """Affiche le code arm correspondant au fait d'envoyer la valeur entière d'une expression sur la sortie standard"""
+    arm_instruction("pop", "{r1}")  # on dépile la valeur d'expression sur r1
+    arm_instruction("ldr", "r0", "=.LC1")
+    arm_instruction("bl", "printf")  # on envoie la valeur de r1 sur la sortie standard
 
 
-"""
-Affiche le code arm correspondant au fait de mettre en pause le programme, et permet à l’utilisateur d’entrée au clavier une chaîne 
-de caractère qui est interprétée comme un entier.
-"""
 def gen_lire():
-    arm_instruction("ldr", "r0", "=.LC0", "", "Charge l’adresse de la chaîne de format pour scanf dans r0")
+    """
+    Affiche le code arm correspondant au fait de mettre en pause le programme, et permet à l’utilisateur d’entrée au clavier une chaîne
+    de caractère qui est interprétée comme un entier.
+    """
+    arm_instruction("ldr", "r0", "=.LC0", comment="Charge l’adresse de la chaîne de format pour scanf dans r0")
     arm_instruction("sub", "sp", "sp", "#4", "Réserve de l’espace sur la pile pour stocker l’entier lu (on fait sp = sp -4)")
-    arm_instruction("movs", "r1" , "sp", "", "Copie l’adresse de cet espace dans r1")
-    arm_instruction("bl", "scanf", "", "", "Lance scanf pour lire l’entier et le stocker à l’adresse spécifiée par r1")
+    arm_instruction("movs", "r1", "sp", "", "Copie l’adresse de cet espace dans r1")
+    arm_instruction("bl", "scanf", comment="Lance scanf pour lire l’entier et le stocker à l’adresse spécifiée par r1")
     arm_instruction("pop", "{r2}", "", "", "Dépiler l'input dans r2")
+
 
 def gen_block_operation(instruction):
     endTrue = arm_nouvelle_etiquette()
     ifFalse = arm_nouvelle_etiquette()
-    
+
     if type(instruction) == arbre_abstrait.While:
         arm_instruction(f"{endTrue}:", comment="true condition jump")
     gen_expression(instruction.cond)
@@ -246,10 +217,9 @@ def gen_block_operation(instruction):
             gen_listeInstructions(instruction.elseInstruction.instructions)
         arm_instruction(f"{endTrue}:", comment="true condition jump")
 
-"""
-Affiche le code arm pour calculer et empiler la valeur d'une expression
-"""
+
 def gen_expression(expression):
+    """Affiche le code arm pour calculer et empiler la valeur d'une expression"""
     if type(expression) == arbre_abstrait.Function:
         instType = gen_instruction(expression)
         arm_instruction("push", "{r2}", "", "", "")
@@ -270,6 +240,7 @@ def gen_expression(expression):
         erreur("type d'expression inconnu " + typeStr(type(expression)))
     return type(expression)
 
+
 def gen_variable(variable):
     inProgram, _ = tableSymboles.has(variable.valeur)
     if not inProgram:
@@ -279,12 +250,9 @@ def gen_variable(variable):
     arm_instruction("push", "{r2}", comment=f"Stack {variable.valeur}")
     return tableSymboles.returnType(variable.valeur)
 
-"""
-Affiche le code arm pour calculer l'opération et la mettre en haut de la pile
-"""
-
 
 def gen_operation(operation):
+    """Affiche le code arm pour calculer l'opération et la mettre en haut de la pile"""
     op = operation.op
 
     type1 = gen_expression(operation.exp1)  # on calcule et empile la valeur de exp1
@@ -297,18 +265,11 @@ def gen_operation(operation):
 
     arm_instruction("pop", "{r0}", "", "", "dépile exp1 dans r0")
 
-    comparisons = {
-        "==": "EQ",
-        "!=": "NE",
-        "<=": "LE",
-        ">=": "GE",
-        "<": "LT",
-        ">": "GT"
-    }
+    comparisons = {"==": "EQ", "!=": "NE", "<=": "LE", ">=": "GE", "<": "LT", ">": "GT"}
     if type1 == arbre_abstrait.Integer and gen_operation_integer(op):
-        True # do nothing
+        True  # do nothing
     elif type1 == arbre_abstrait.Boolean and gen_operation_boolean(op):
-        True # do nothing
+        True  # do nothing
     elif op in comparisons.keys():
         labelTrue = arm_nouvelle_etiquette()
         labelFalse = arm_nouvelle_etiquette()
@@ -325,29 +286,18 @@ def gen_operation(operation):
     arm_instruction("push", "{r0}", "", "", "empile le résultat")
     return type1
 
+
 def gen_operation_integer(op):
     code = {
         "+": "add",
-        "*": "mul"
+        "*": "mul",
     }  # Un dictionnaire qui associe à chaque opérateur sa fonction arm
     # Voir: https://developer.arm.com/documentation/dui0497/a/the-cortex-m0-instruction-set/instruction-set-summary?lang=en
 
     if op in code.keys():
-        arm_instruction(
-            code[op],
-            "r0",
-            "r1",
-            "r0",
-            "effectue l'opération r0" + op + "r1 et met le résultat dans r0",
-        )
+        arm_instruction(code[op], "r0", "r1", "r0", "effectue l'opération r0" + op + "r1 et met le résultat dans r0")
     elif op == "-":
-        arm_instruction(
-            "sub",
-            "r0",
-            "r0",
-            "r1",
-            "effectue l'opération r0" + op + "r1 et met le résultat dans r0",
-        )
+        arm_instruction("sub", "r0", "r0", "r1", "effectue l'opération r0" + op + "r1 et met le résultat dans r0")
     elif op == "/":
         arm_instruction("bl", "__aeabi_idiv")
     elif op == "%":
@@ -357,15 +307,10 @@ def gen_operation_integer(op):
         return False
     return True
 
+
 def gen_operation_boolean(op):
     if op == "et":
-        arm_instruction(
-            "mul",
-            "r0",
-            "r1",
-            "r0",
-            "effectue l'opération r0" + op + "r1 et met le résultat dans r0",
-        )
+        arm_instruction("mul", "r0", "r1", "r0", "effectue l'opération r0" + op + "r1 et met le résultat dans r0")
     elif op == "ou":
         arm_instruction("add", "r0", "r1", "r0")
         arm_instruction("cmp", "r0", "#2")
@@ -377,6 +322,7 @@ def gen_operation_boolean(op):
     else:
         return False
     return True
+
 
 if __name__ == "__main__":
     afficher_arm = True
